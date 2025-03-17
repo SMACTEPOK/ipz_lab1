bea0243a874979450a9461211f15ddd70c03829d
Oleksandr Korniienko <sasakornienko78@gmail.com>
2025-03-18 01:55:31 +0200

    Додав lab_1.java

---
+++ lab_1.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+class Input
+{ 
+  public static void main(String args[]) throws IOException 
+  { 
+    System.out.println("Korniienko Oleksandr \nFirst steps in software engineering");  
+  }
+}
